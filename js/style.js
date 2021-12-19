1d20e5091ff46ba9ccba17873ca2fe184ab0f493
HabibIbrahimov <habibyi@code.edu.az>
2021-12-19 14:41:37 +0400

    feat:created Accordions page

--- js/style.js
+++ js/style.js
@@ -25,4 +25,20 @@ $(document).on('click', '.mobile-navbar ul li a', function () {
 })
 
 
+
+//ACCORDION
+
+$(document).on('click', '.question', function()
+    {   
+       $(this).siblings('.question').children('i').removeClass('fa-minus').addClass('fa-plus');
+       $(this).siblings('.answer').not($(this).next()).slideUp();
+       $(this).children('i').toggleClass('fa-plus').toggleClass('fa-minus');
+       $(this).next().slideToggle();
+       $(this).siblings('.active').removeClass('active');
+       $(this).toggleClass('active');
+    })
 });
+
+
+
+
