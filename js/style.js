907b6a079c2d2235c456cd3ab08ed5923342cb45
HabibIbrahimov <habibyi@code.edu.az>
2021-12-20 14:46:53 +0400

    feat:created tabs page

--- js/style.js
+++ js/style.js
@@ -37,8 +37,42 @@ $(document).on('click', '.question', function()
        $(this).siblings('.active').removeClass('active');
        $(this).toggleClass('active');
     })
-});
 
+
+ // TAB
+
+      $(document).on('click', 'ul li', function()
+      {   
+          $(this).siblings('.active').removeClass('active');
+          $(this).addClass('active');
+          let dataId = $(this).attr('data-id');
+          $(this).parent().next().children('p.active').removeClass('active');
+  
+          $(this).parent().next().children('p').each(function()
+          {
+              if(dataId == $(this).attr('data-id'))
+              {
+                  $(this).addClass('active')
+              }
+          })
+      })
+  
+      $(document).on('click', '.tab4 ul li', function()
+      {   
+          $(this).siblings('.active').removeClass('active');
+          $(this).addClass('active');
+          let dataId = $(this).attr('data-id');
+          $(this).parent().parent().next().children().children('p.active').removeClass('active');
+  
+          $(this).parent().parent().next().children().children('p').each(function()
+          {
+              if(dataId == $(this).attr('data-id'))
+              {
+                  $(this).addClass('active')
+              }
+          })
+      })
+});
 
 
 
