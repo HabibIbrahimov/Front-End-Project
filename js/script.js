4c47b5815415b7f25e6d8aefe8106572b67ae725
HabibIbrahimov <habibyi@code.edu.az>
2021-12-21 10:12:30 +0400

    feat:created progressbar page

--- js/script.js
+++ js/script.js
@@ -72,6 +72,20 @@ $(document).on('click', '.question', function()
               }
           })
       })
+
+
+      // Progress Bar Page 
+$('.loading').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2500,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
 });
 
 
